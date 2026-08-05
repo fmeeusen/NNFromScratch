4310b6f152d6e153719640cad81067b0335a8059
fmeeusen <friso@meeusen.eu>
2026-08-05 13:18:29 +0200

    Clean up the test suite.

--- nn_test.py
+++ nn_test.py
@@ -3,28 +3,25 @@ import neuron
 import numpy as np
 
 class Test_InitialParameters(unittest.TestCase):
-    def test_number_of_parameters(self):
-        layer_structure = [2,3,4,5,6]
-        weights,biases=neuron.generate_parameters(layer_structure)
-        flat_weights = np.concatenate(weights)
-        flat_biases = np.concatenate(biases)
+    def setUp(self):
+        self.layer_structure = [2, 3, 4, 5, 6]
+        weights, biases = neuron.generate_parameters(self.layer_structure)
+        self.flat_weights = np.concatenate(weights)
+        self.flat_biases = np.concatenate(biases)
+        self.flat_params = np.concatenate([self.flat_weights, self.flat_biases])
 
-        self.assertEqual(len(flat_weights), 2*3+3*4+4*5+5*6)
-        self.assertEqual(len(flat_biases), 3+4+5+6)
+    def test_flat_weights_and_biases_have_expected_length(self):
+        expected_weights = sum(self.layer_structure[i] * self.layer_structure[i+1] for i in range(len(self.layer_structure)-1))
+        expected_biases = sum(self.layer_structure[1:]) 
+        self.assertEqual(len(self.flat_weights), expected_weights)
+        self.assertEqual(len(self.flat_biases), expected_biases)
 
-    def test_if_all_unique_elements(self):
-        layer_structure = [2,2,3,3]
-        weights,biases=neuron.generate_parameters(layer_structure)
-        flat_weights = np.concatenate(weights)
-        flat_biases = np.concatenate(biases)
-        flat_params = np.append(flat_weights, flat_biases)
-
+    def test_if_all_elements_are_unique(self):
         # Sort the parameters and then take the differences between neighbouring elements 
-        # This is a computationally cheaper method of checking for non-unique elements
-
-        sorted_params = np.sort(flat_params)
+        # This is a computationally cheaper method of checking for non-unique elements than when going through all the elements
+        sorted_params = np.sort(self.flat_params)
         diffs = np.diff(sorted_params)
-        no_of_duplicates = np.sum(diffs <= 1e-5)
+        no_of_duplicates = np.sum(diffs <= 1e-6)
 
         self.assertEqual(
             no_of_duplicates, 0,
